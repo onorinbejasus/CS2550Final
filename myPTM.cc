1938186cc1eb67af147b459096994969e30e6040
William Francis <wllm.francis@gmail.com>
2013-04-18 23:21:05 -0400

    [myPTM CC] started defining the PTM

--- myPTM.cc
+++ myPTM.cc
@@ -1,1 +1,40 @@
 #include "myPTM.hh"
+
+myPTM::myPTM()
+{
+    
+}
+
+myPTM::myPTM(vector<string> cmd)
+{
+    int i = 0;
+    for(i = 0; i < cmd.size(); i++)
+    {
+        string subCmd = cmd.get(i);
+        //execute subCmd
+    }
+}
+
+int myPTM::lockRows(int start, int end)
+{
+    m_start = start;
+    m_end = end;
+    
+    
+    return -1;
+}
+
+int myPTM::unlockRows(void)
+{
+    m_start = -1;
+    m_end = -1;
+    
+    return -1;
+}
+
+int myPTM::writeOut(void* buffer, void* dataset)
+{
+    
+    
+    return -1;
+}
--- myPTM.hh
+++ myPTM.hh
@@ -1,7 +1,26 @@
+#ifndef _MYPTM_H_
+#define _MYPTM_H_
+
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+//#include ""
+
 class myPTM{
 	
 	public:
 		myPTM(){}; // default constructor
+        myPTM(vector<string> cmd){};
 	private:
-	
+        float m_tran_id;
+        int m_start;
+        int m_end;
+    
+        int lockRows(int start, int end){};
+        int unlockRows(void){};
+        int writeOut(void* buffer,void* dataset){};
 };
+
+#endif
