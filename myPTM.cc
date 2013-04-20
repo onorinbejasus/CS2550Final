37a9883228ed4a9125cac106fb47a7efaf599ad6
William Francis <wllm.francis@gmail.com>
2013-04-19 22:33:00 -0400

    [PTM CC] added lock file wrapper.

--- myPTM.cc
+++ myPTM.cc
@@ -1,5 +1,13 @@
 #include "myPTM.hh"
 
+/*
+  Point of transactions:
+  Grouping a set of operations together such that they are not interrupted.  Essentially locking the files until the transaction is done.
+ 
+  
+ 
+*/
+
 myPTM::myPTM()
 {
     
@@ -7,12 +15,57 @@ myPTM::myPTM()
 
 myPTM::myPTM(vector<string> cmd)
 {
+    //LOCK FILE OR WAIT
+    FILE * lf = fopen("lockfile","r+");
+    char* lock;
+    fread(lock,sizeof(char), 1, lf);
+    if(strncmp(lock,"0")==0)
+    {
+        fseek(lf,0, SEEK_SET);
+        fwrite("1", sizeof(char), 1, lf);
+    }
+    else
+    {
+        //if it IS already 1.
+        //WAIT();
+    }
+    fclose(lf)
+    
+    //execute tasks
     int i = 0;
+    int retVal = 0;
     for(i = 0; i < cmd.size(); i++)
     {
         string subCmd = cmd[i];
-        //execute subCmd
+        //int = execute subCmd
+        if(retVal == -1)
+        {
+            break;
+        }
     }
+    if (retVal == -1 )
+    {
+        //rollback
+    }
+    else
+    {
+        //carry out injections.
+    }
+    
+    //unlock the file.
+    lf = fopen("lockfile","r+");
+    fread(lock,sizeof(char), 1, lf);
+    if(strncmp(lock,"1")==0)
+    {
+        fseek(lf,0, SEEK_SET);
+        fwrite("0", sizeof(char), 1, lf);
+    }
+    else
+    {
+        //a fatal error.  Somehow the lock was released before it should have.
+        //THROW SOME SORT OF FLAG OR ERROR
+    }
+    fclose(lf)
 }
 
 int myPTM::lockRows(int start, int end)
