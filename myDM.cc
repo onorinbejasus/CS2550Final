a3206421de8eb8fe2a29775e81660d6a00f384b9
William Francis <wllm.francis@gmail.com>
2013-04-21 23:05:07 -0400

    [myDM CC]created the class for the datamanager.

--- myDM.cc
+++ myDM.cc
@@ -1,1 +1,69 @@
 #include "myDM.hh"
+#include <stdio.h>
+
+myDM::myDM()
+{
+    
+}
+myDM::myDM(int searchMode, long bufferSize)
+{
+    _SEARCH_MODE_ = searchMode;
+}
+
+
+int myDM::read(char* filename, void* id)
+{
+    //if(!exists(filename)) return -2;
+    if(!exists(filename)) return -2;
+    //if(!rExists(record)) return -1;
+    //retrieve records with areacode in filename.
+}
+
+
+int myDM::multRead(char* filename, void* areaCode)
+{
+    //if(!exists(filename)) return -2;
+    if(!exists(filename)) return -2;
+    //if(!rExists(record)) return -1;
+}
+
+//write record to file.
+void* myDM::write(char* filename, void* record)
+{
+    FILE* fp;
+    //if(!exists(filename)) create the file.
+    if(!exists(filename))
+    {
+        fp = fopen(filename, "w+");
+        fclose(fp);
+    }
+    //fp = fopen(filename,"r+");
+    //write record
+}
+
+//delete filename
+void* myDM::deleteData(char* filename)
+{
+    remove(filename);
+}
+
+//if filename exists return true, else false;
+bool myDM::exists(char* filename)
+{
+    //if file exists, return true.
+    //else return false.
+    FILE* fp = fopen(filename, "r+");
+    if(fp != NULL){
+        fclose(fp);
+        return true;
+    }
+    return false;
+}
+
+//if record exists, return true, else false
+bool myDM::rExists(char*filename,void* record)
+{
+    FILE* fp = fopen(filename,"r");
+    //search for record.
+    fclose(fp);
+}
--- myDM.hh
+++ myDM.hh
@@ -1,7 +1,15 @@
 class myDM{
 	
 	public:
-		myDM(){}; // Default constructor
+		myDM(); // Default constructor
+        myDM(int searchMode, long bufferSize);
 	private:
+        int _SEARCH_MODE_;
+        int read(char*filename, void*id);
+        int multRead(char*filename,void* areaCode);
+        void* write(char* filename, void* record);
+        void* deleteData(char* filename);
+        bool exists(char*filename);
+        bool rExists(char*filename,void*record);
 	
 };
